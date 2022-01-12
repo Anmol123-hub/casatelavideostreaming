e951b2dffb7db3dbf840c83c9a9c02f7d920c52a
Anmol <anmolvarshney98@gmail.com>
2022-01-12 13:36:10 +0530

    for website

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000
 const server = http.createServer((req, res) => {})
 
 server.listen(port, () => {
-    console.log("Listening on port"+ port)
+    console.log(`Listening on port ${port}`)
 })
 
 const webSocket = new Socket({ httpServer: server })
